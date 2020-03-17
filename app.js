572411e567b1b181299e7ea98820565083c22b4e
Bino Nguyen <binonguyen1090@gmail.com>
2020-03-17 10:57:46 -0700

    add UserAuth before validations

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const User = require('./models/User');
+const users = require("./routes/api/users");
+const forms = require("./routes/api/forms");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Hello World"));
 
