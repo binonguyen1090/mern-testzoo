9abbd7ea161de2c990c7493d9277bc3e91dcb6d0
Bino Nguyen <binonguyen1090@gmail.com>
2020-03-18 11:29:35 -0700

    UserAuth frontend and backend functionality

---
+++ frontend/src/reducers/errors_reducer.jsx
@@ -0,0 +1,10 @@
+// src/reducers/errors_reducer.js
+
+import { combineReducers } from "redux";
+
+import SessionErrorsReducer from "./session_errors_reducer";
+// import ErrorsReducer from "./errors_reducer"
+export default combineReducers({
+  session: SessionErrorsReducer,
+  // error: ErrorsReducer
+});
