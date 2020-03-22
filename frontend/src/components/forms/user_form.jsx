80875447a3fce6fc23a68d5eafbb8e60e9c4c349
Dawinder Singh <dawindernsingh@gmail.com>
2020-03-22 15:12:57 -0700

    finish user forms

--- frontend/src/components/forms/user_form.jsx
+++ frontend/src/components/forms/user_form.jsx
@@ -25,20 +25,27 @@ export default class UserForm extends React.Component {
   render() {
     const form = this.props.forms.map(form => (
       <div key={form.id}>
-        <Link to={`/forms/${form._id}`} >
-          {form.title}
+        <Link id='link-forms' to={`/forms/${form._id}`} >
+          <div className='form-index-form'>
+            <strong>{form.title}</strong>
+            <div id='form-footer'>
+              <div>{form.category}</div>
+              <button onClick={this.handleClick} value={form._id}>Delete?</button>
+            </div>
+          </div>
         </Link>
-        <button onClick={this.handleClick} value={form._id}>DELETE</button>
       </div>
     ));
 
     return (
       <div className='user-form-out'>
         <div id='user-form-header'>
-          <h1>Your Forms</h1>
+          <h1>Your Tests</h1>
           <Link className='back-btn' to="/">Go back</Link>
         </div>
-        <div key={form.id} >{form}</div>
+        <div id='form-item-padding'>
+          <div className='form-item' key={form.id} >{form}</div>
+        </div>
       </div>
     );
   }
