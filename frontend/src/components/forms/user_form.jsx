21d9840d807786583a2cc5941bb94c3374c1fff4
Dawinder Singh <dawindernsingh@gmail.com>
2020-03-22 14:21:45 -0700

    edit view form functionality

--- frontend/src/components/forms/user_form.jsx
+++ frontend/src/components/forms/user_form.jsx
@@ -2,45 +2,44 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './user_form.css'
+import NavBarContainer from "../nav/navbar_container";
 
 export default class UserForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this)
-    }
+  constructor(props) {
+      super(props);
+      this.handleClick = this.handleClick.bind(this)
+  }
 
-    componentDidMount() {
-        this.props.fetchUserForms(this.props.currentUserID);
-    }
+  componentDidMount() {
+      this.props.fetchUserForms(this.props.currentUserID);
+  }
 
 
-    handleClick(e){
-        e.preventDefault()
-        const formId = e.currentTarget.value
-        this.props.destroyForm(formId)
-    }
-   
-    render() {
-        const form = this.props.forms.map(form => (
-          <li key={form.id}>
-            <Link to={`/forms/${form._id}`} >
-              {form.title}
-            </Link>
-            <button onClick={this.handleClick} value={form._id}>DELETE</button>
-          </li>
-        ));
+  handleClick(e){
+      e.preventDefault()
+      const formId = e.currentTarget.value
+      this.props.destroyForm(formId)
+  }
+  
+  render() {
+    const form = this.props.forms.map(form => (
+      <div key={form.id}>
+        <Link to={`/forms/${form._id}`} >
+          {form.title}
+        </Link>
+        <button onClick={this.handleClick} value={form._id}>DELETE</button>
+      </div>
+    ));
 
-        return (
-          <div>
-            <h1>My Forms</h1>
-            <div>
-              <ul key={form.id} >{form}</ul>
-            </div>
-            <button>
-              <Link to="/">Back</Link>
-            </button>
-          </div>
-        );
-    }
+    return (
+      <div className='user-form-out'>
+        <div id='user-form-header'>
+          <h1>Your Forms</h1>
+          <Link className='back-btn' to="/">Go back</Link>
+        </div>
+        <div key={form.id} >{form}</div>
+      </div>
+    );
+  }
 }
