b87cf896919ae34d2fe5eb63e546e9f41b33e385
Dawinder Singh <dawindernsingh@gmail.com>
2020-04-02 16:00:38 -0700

    fix show-form errors and btns

--- frontend/src/components/forms/show_form.jsx
+++ frontend/src/components/forms/show_form.jsx
@@ -1,7 +1,7 @@
 
-
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './show_form.css';
 
 import QuestionsIndex from "../questions/questions_index_container"
 
@@ -17,23 +17,30 @@ export default class ShowForm extends React.Component {
     render() {
 
         return (
-          <div>
-            <h1>Show Forms</h1>
-            <h1>{this.props.form.title}</h1>
-            <h1>{this.props.form.category}</h1>
-            <div>
-              <QuestionsIndex
-                key={this.props.form_id}
-                formId={this.props.form_id}
-              />
-            </div>
-            <div>
-              <button>
-              <Link to={`/questions`}>Create Questions</Link>
-              </button>
-              <button>
-                <Link to={`/users/${this.props.currentUserID}`}>Back</Link>
-              </button>
+          <div className="create-form">
+            <Link
+              className="back-btn2" to={`/users/${this.props.currentUserID}`}>Go Back
+            </Link>
+
+            <div className="form-show">
+              <div id="form-top">
+                <strong>{this.props.form.title}</strong>
+                <div id='form-top2'>
+                  <Link className='create-question' to={`/questions`}>Create A New Question</Link>
+                  <div>Category: {this.props.form.category}</div>
+                </div>
+              </div>
+              <div className="all-questions">
+                <QuestionsIndex
+                  key={this.props.form_id}
+                  formId={this.props.form_id}
+                />
+              </div>
+              <div>
+                <button>
+                  <Link to={`/questions`}>Create Questions</Link>
+                </button>
+              </div>
             </div>
           </div>
         );
