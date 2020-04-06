54c7e73c8814e562f72dcfce00d02108f9c6a7aa
Joe Xiyang Lu <joe.luxiyang@gmail.com>
2020-04-05 21:49:57 -0700

    Update game_form_container.jsx

--- frontend/src/components/forms/game_form_container.jsx
+++ frontend/src/components/forms/game_form_container.jsx
@@ -5,7 +5,6 @@ import { fetchForm } from '../../actions/form_actions';
 import { startGame, fetchGames } from "../../actions/game_actions";
 
 const mSTP = (state, ownProps) => {
-    debugger
     return {
         form_id: ownProps.match.params.form_id,
         currentUser: state.session.user,
